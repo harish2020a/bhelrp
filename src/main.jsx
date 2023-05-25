b77c54b7597fa7b23389cb25a270d421f0ca3e35
harish2020a <harish.2020a@vitstudent.ac.in>
2023-05-25 11:30:41 +0530

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import AuthContextProvider from "./store/auth-context";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-    <App />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
